f854cbca00b68f5d6d705f28affa624845abe6ac
RohitG28 <rohit.ghivdonde@gmail.com>
2018-02-23 10:18:31 +0530

    Final Changes

--- mapReduce.cpp
+++ mapReduce.cpp
@@ -1,7 +1,6 @@
 #include "mpi.h"
 #include <iostream>
 #include <math.h>
-#include <time.h>
 #include <stdio.h>
 #include <bits/stdc++.h>
 
@@ -23,43 +22,34 @@
 #include <sstream>
 #include <fstream>
 
-//Can be used later for sort
+//For sorting vector
 #include <algorithm>
 
-// cereal libraries
+//cereal libraries
 #include "cereal/types/map.hpp"
 #include "cereal/types/vector.hpp"
 #include "cereal/types/string.hpp"
 #include "cereal/types/utility.hpp"
 #include "cereal/archives/binary.hpp"
 
-
 using namespace std;
 
 /************************************************************************************************************************************
-
-1. Assume each process is mapped to one node.
-2. Assuming the query can only be of the form -- give the list of documents containing the word in decreasing ordre of frequency.
-   If the query is of the form -- whether the document contains this word or not? - we have travese whole list of documnet ids 
-   associated with a word.
-3. IMPLEMENT : EFFICIENT IO
-4. To compile: mpiCC -Iinclude filename -std=c++11
-5. DOCID 
-6. partition
-7. document size
-8. special characters
-
+To compile: mpiCC -Iinclude filename -std=c++11
+To run: Give the path to the folder containing the data as an argument
 ************************************************************************************************************************************/
 
-#define MAX_FILE_NAME_SIZE 30
+#define MAX_FILE_NAME_SIZE 100
 #define LOWEST_ALPHABET_ASCII 97
 #define LONG_DIGIT_NO 10
 
+//Comparator function to sort vectors in descending order
 bool sortinrev(const pair<long int,string> &a, const pair<long int,string> &b)
 {
        return (a.first > b.first);
 }
 
+//Merge two sorted vectors into one again in descending order 
 vector<pair<long int, string>> mergeVectors(vector<pair<long int, string>> vec1,vector<pair<long int, string>> vec2)
 {
 	vector<pair<long int, string>> mergedVec(vec1.size()+vec2.size());
@@ -78,10 +68,10 @@ void printMaps(vector<unordered_map<string, vector<pair<long int,string>>>> rece
 	unordered_map<string, vector<pair<long int,string>>> final_map;
 	unordered_map<string, vector<pair<long int,string>>>::iterator mapItr;
 
-	//Postings list for a word
+	//Postings list(a list of freq + docID) for a word
 	vector<pair<long int,string>> documentsWithWord,mergedVector;
 
-	//An entry for a map ---- word and its postings list
+	//An entry for a map -> word and its postings list
 	string currentWord;
 	vector<pair<long int , string>> postingsForWord;
 
@@ -132,19 +122,17 @@ void printMaps(vector<unordered_map<string, vector<pair<long int,string>>>> rece
 /***********************************************************************************************************************/
 
 
-
 int main(int argc, char** argv)
 {
 	int err;
 	int processId,noOfProcesses;
 	string currentFolder = ".";
 	string parentFolder = "..";
+	
+	//To calculate total time and the time required to make local indices
 	double elapsedTime,elapsedTimeLocal;
 
-	//Hard Coded Variable for the time being
-	long int noOfFiles = 3;
-
-	//Word Count Map for each document
+	//Word Count Map for each document : stores a word and its frequency in the document
    	unordered_map<string, long int> wordCountMap;
 
 	//Parallelization starts
@@ -154,13 +142,15 @@ int main(int argc, char** argv)
 	err = MPI_Comm_rank(MPI_COMM_WORLD, &processId);
 	err = MPI_Comm_size(MPI_COMM_WORLD, &noOfProcesses);
 	
+	//Time calculation starts
 	elapsedTime = -MPI_Wtime();
 	elapsedTimeLocal = -MPI_Wtime();
 
-   	//Inverted Index Map for whole documents in the local node
-   	vector<unordered_map<string, vector<pair<long int,string>>>> invertedIndexMap(noOfProcesses);
 
-	///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/********************************************Mapping Phase Begins*******************************************************/	
+   	
+   	//A vector of Inverted Index Maps for whole documents in the local node
+   	vector<unordered_map<string, vector<pair<long int,string>>>> invertedIndexMap(noOfProcesses);
 
 	//Load Stopwords into the set
 	//https://algs4.cs.princeton.edu/35applications/stopwords.txt//
@@ -169,10 +159,8 @@ int main(int argc, char** argv)
 	stopwordsStream.open("stopwords.txt");
 	string stopWord;		
 
-	while(!stopwordsStream.eof())
+	while(getline(stopwordsStream, stopWord))
 	{
-	   	getline(stopwordsStream, stopWord);
-
 	   	if(stopWord.empty())
 	   	{
 	   		continue;
@@ -186,20 +174,20 @@ int main(int argc, char** argv)
 	   	stopwords.insert(stopWord);
 	}	
 	
-	/////////////////////////////////////////////////////////////////////////////////////////////////////////////// 		
-
-	//Mappers
+	//Stores the name of file to be opened for indexing
 	char filename[MAX_FILE_NAME_SIZE];
 
 	//There is a directory for each node
 	char directoryName[MAX_FILE_NAME_SIZE];
-	sprintf(directoryName,argv[1]);
+	char* directory = argv[1];
+	sprintf(directoryName,directory);
 
 	DIR *dp; 
 	struct dirent *ep;    
 	 
 	dp = opendir(directoryName);
 
+	//Partitioning Words according to the first letter of the word 
 	int partitionSize = 26/(noOfProcesses);
 	int remainder = 26%(noOfProcesses);
 	vector<pair<long int,string>>::iterator vecItr;
@@ -222,8 +210,10 @@ int main(int argc, char** argv)
 		}
 	}
 
-	//partition to which a given word belongs
+	//Partition to which a given word belongs
 	int correspondingPartition;
+
+	//Variable declarations
 	long int documentNo, wordFreq;
 	ifstream inputFile;
 	string readLine;
@@ -233,10 +223,10 @@ int main(int argc, char** argv)
     vector<pair<long int,string>> documentsWithWord,mergedVector;
     string documentID;
     
-
 	if (dp != NULL)
 	{
 		documentNo = 0;
+
 		//Start creating the local index in the node -- read each file one by one
 		while ((ep = readdir(dp))!=NULL)
 		{
@@ -256,10 +246,9 @@ int main(int argc, char** argv)
 			s.str().clear();
 
 			//Current file read
-
+			sprintf(directoryName,directory);
 			sprintf(filename,"%s/%s",directoryName,ep->d_name);
 			inputFile.open(filename);
-
 
 		 	while(getline(inputFile, readLine))
 			{
@@ -269,6 +258,8 @@ int main(int argc, char** argv)
 			    }
 
 			    char* lineString = &readLine[0];
+			   	
+			   	//Tokenize the line using punctuation marks
 			    char* token = strtok(lineString,",./;-!?@&(){}[]<>:'\" \r");
 
 			    while(token != NULL)
@@ -295,7 +286,7 @@ int main(int argc, char** argv)
    			//Closing the file
    			inputFile.close();
 
-   			//When the document is processed and the file is closed
+   			//Here the document is processed and the file is closed
 
 		    //Start adding the words from the word count map to the inverted index map
 		    for (mapItr= wordCountMap.begin(); mapItr != wordCountMap.end(); mapItr++)
@@ -303,6 +294,7 @@ int main(int argc, char** argv)
 	        	currentWord = mapItr->first;
 	        	wordFreq = mapItr->second;
 
+	        	//If the first letter is a number then put the word into last partition since last partition generally contains less words
 	        	if(currentWord[0] >= 48 && currentWord[0] <= 57)
 	        		correspondingPartition = noOfProcesses-1;
 	        	else
@@ -311,6 +303,7 @@ int main(int argc, char** argv)
 	        	//If the currentWord doesn't exist in invertedIndexMap
 	        	if(invertedIndexMap[correspondingPartition].find(currentWord) == invertedIndexMap[correspondingPartition].end())
 	        	{
+	        		//Create a new vector corresponding to that word
 	        		vector<pair<long int,string>> newVector;
 	        		newVector.push_back(make_pair(wordFreq,documentID)); 
 	        		invertedIndexMap[correspondingPartition][currentWord] = newVector;
@@ -324,13 +317,10 @@ int main(int argc, char** argv)
 	        	}
 	        }
 
-	        // cout << "One Document over from Process: "<<filename<<" : " <<processId << endl;
-	        
 	        //Clearing the wordCountMap for processing new document
 	        wordCountMap.clear();
 
 	        //Now go to the next document in next iteration
-	        
 		}
 	    
 	    //Close the directory
@@ -341,8 +331,10 @@ int main(int argc, char** argv)
 	    perror("Couldn't open the directory");
 	}
 
+	//Local Indices extraction done
 	elapsedTimeLocal += MPI_Wtime();
 
+	//Print time taken to extract local indices
 	printf("Local Index time: %lf\n",elapsedTimeLocal);
 
 	/****************************************************Sorting the vectors in the local Map******************************************/
@@ -357,49 +349,57 @@ int main(int argc, char** argv)
 		}
 	}
 
-	//Only for debugging - i.e printing local index into the files
-	// printMaps(invertedIndexMap, noOfProcesses, processId, "LocalIndex");
-
 	//*******************************************ALL DOCUMENTS IN NODE ARE PROCESSESED****************************************//
 
 
-
 	/********************************************************* COMMUNICATION BEGINS ***********************************************/
-
-
-
 	
-	//send/receive the invertedIndexMap vector to/from other process
-
 	//******************** if documents size less ************************//
-
 	if(0)
 	{
+		//Here the communication uses serialized strings
+
+		//String Stream to store the serialized archive of the maps
 		stringstream ss;
 		cereal::BinaryOutputArchive outArchive(ss);
 
+		//Array containing the serialized string for maps corresponding to each partition
 		string serializedMap[noOfProcesses];
 		int serializedStringSizes[noOfProcesses];
+
+		//All strings concatenated to form on combined string
 		string combinedSerializedMapString;
+
+		//Displacement from start location from where to take the data for sending/receiving to/from each process
 		int sendDisp[noOfProcesses];
 		int receiveDisp[noOfProcesses];
+
+		//Size of data received from each process
 		int receiveSizes[noOfProcesses];
+
+		//Total data to be received
 		int totalReceiveSize = 0;
 		int previousStringSize = 0;
 
 		for(int k=0;k<noOfProcesses;k++)
 		{
+			//Serialize the map
 			outArchive(invertedIndexMap[k]);
+
+			//Extract the string from the string stream
 			serializedMap[k] = ss.str();
 			serializedStringSizes[k] = serializedMap[k].size();	
 			combinedSerializedMapString += serializedMap[k];
 		
 			sendDisp[k] = previousStringSize;
 			previousStringSize += serializedStringSizes[k];
+			
+			//Clear the string stream
 			ss.str("");
 			ss.clear();
 		}
 		
+		//Send/Receive the sizes of data(length of the string) to be sent/received to/from each process
 		MPI_Alltoall(&(serializedStringSizes[0]), 1, MPI_INT, &(receiveSizes[0]), 1, MPI_INT, MPI_COMM_WORLD);
 
 		previousStringSize = 0;
@@ -410,17 +410,23 @@ int main(int argc, char** argv)
 			previousStringSize += receiveSizes[k];	
 		}
 		
+		//Combined string to receive the strings from each process
 		string receivedSerializedMapString;
 		receivedSerializedMapString.resize(totalReceiveSize);
 
+		//Send/Receive strings to/from all other processes
 		MPI_Alltoallv(&(combinedSerializedMapString[0]), &(serializedStringSizes[0]), &(sendDisp[0]), MPI_CHAR, &(receivedSerializedMapString[0]), &(receiveSizes[0]), &(receiveDisp[0]), MPI_CHAR, MPI_COMM_WORLD);
 
 		previousStringSize = 0;
+
+		//String stream to extract the maps
 		stringstream newSS;
 		cereal::BinaryInputArchive inArchive(newSS);	
 		for(int k=0;k<noOfProcesses;k++)
 		{
 			newSS << receivedSerializedMapString.substr(previousStringSize,receiveSizes[k]);
+
+			//Extract the map from the serialized string
 			cereal::BinaryInputArchive inArchive(newSS);
 			inArchive(invertedIndexMap[k]);
 			previousStringSize += receiveSizes[k];
@@ -431,9 +437,12 @@ int main(int argc, char** argv)
 
 	//**************** end ************************//
 
-	//**************** large sized documents ************************//
+	//**************** large database ************************//
 	else
 	{
+		//Here the communication uses file I/O
+
+		//File stream to store the serialized maps in binary form
 		ofstream serializeFile;
 		cereal::BinaryOutputArchive outArchive(serializeFile);
 		
@@ -444,6 +453,7 @@ int main(int argc, char** argv)
 			serializeFile.close();
 		}
 		
+		//Wait till all the processes are done making their respective files containing serialized maps
 		MPI_Barrier(MPI_COMM_WORLD);
 
 		ifstream inputSerializeFile;
@@ -456,22 +466,17 @@ int main(int argc, char** argv)
 		}
 	}
 
-	// printMaps(invertedIndexMap, noOfProcesses, processId, "Merged");
-
 	//**************** end ************************//
 
 	
-	//*****************************************************REDUCE PHASE BEGINS******************************************************//
+	/*****************************************************REDUCE PHASE BEGINS******************************************************/
 	
-	//New Implementation
 	unordered_map<string, vector<pair<long int,string>>> final_map, localMap;
 	
-	//Received Maps from AllGatherV
-	//vector<unordered_map<string, vector<pair<long int,string>>>> receivedMaps (noOfProcesses);
-
+	//Maps received from all processes
 	vector<unordered_map<string, vector<pair<long int,string>>>> receivedMaps = invertedIndexMap;
 	
-	//An entry for a map ---- word and its postings list
+	//An entry for a map -> word and its postings list
 	vector<pair<long int , string>> postingsForWord;
 
 
@@ -506,13 +511,17 @@ int main(int argc, char** argv)
 		}	
 
 	}
+
+	//Total time calculation done (global indices computed)
 	elapsedTime += MPI_Wtime();
-	/*****************************************************Final Map is Ready********************************************/
+
+	/*****************************************************Final Map Ready*********************************************************/
 
 	//Now write it into file
 
 	//Assuming the distributed global index files are in a folder and file name will be the process id
 
+	//Name of the folder
 	string globalIndexFolder = "GlobalIndex";
 	
 	sprintf(filename,"%s/%d.txt",globalIndexFolder.c_str(),processId);
@@ -545,7 +554,7 @@ int main(int argc, char** argv)
 
 	fclose(fp);
 	
-	printf("Total time: %lf",elapsedTime);
+	printf("Total time: %lf\n",elapsedTime);
 
 	err = MPI_Finalize();
 	return 0;
